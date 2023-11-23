b409be36227ee99113f7d74f38c426e8a9ea64f2
manoj-gowda <manojsurya5616@gmail.com>
2023-11-23 15:57:42 +0530

    tailwind and github config
    - Configured Tailwind
    - Added GitHub issue templates
       - Bug report
       - Feature requests

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-red-500">Drumroll Music</div>;
+  return <div className="text-red-500">Music-Mingle</div>;
 }
