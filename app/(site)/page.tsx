02b1b6667beea6f1d5660eb286c3d0525d194f5c
manoj-gowda <manojsurya5616@gmail.com>
2023-11-23 16:30:06 +0530

    Created header skeleton
    - Displays back and forward buttons on desktop
    - Displays home and search buttons on mobile
    - Always displays authentication buttons

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import Image from "next/image";
 
 export default function Home() {
@@ -13,7 +14,43 @@ export default function Home() {
         overflow-y-auto
       "
     >
-      <Header>Header</Header>
+      <Header>
+        {" "}
+        <div className="mb-2">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            "
+          >
+            Welcome back
+          </h1>
+          <div
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            "
+          >
+            <ListItem
+              name="Liked Songs"
+              image="/images/liked.png"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+        List of songs{" "}
+      </div>
     </div>
   );
 }
