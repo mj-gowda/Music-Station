2f2394416f061fe475be24b5184fb75e988e057e
manoj-gowda <manojsurya5616@gmail.com>
2023-11-25 14:09:29 +0530

    Supabase provider to the whole app
    - Installed required packages for Supabase authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
